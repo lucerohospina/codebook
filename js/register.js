1a86c195ee28c3626791751bc7e19292132b81bb
andrea-isabel <andrea19.93.15@gmail.com>
2018-01-08 18:08:20 -0500

    agregando usuarios a firebase

--- js/register.js
+++ js/register.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
   // Declarando variables
   var $regBtn = $('#reg-btn');
-  var $emailReg = $('#inputEmail2').val();
-  var $passwordReg = $('#inputPassword2').val();
 
   // Asociando eventos
-  $regBtn.on('click', register);
+  $regBtn.click(register);
 
   // Funciones
 
-  function register(event) {
-    event.preventDefault();
+  function register() {
+    var $emailReg = $('#inputEmail').val();
+    var $passwordReg = $('#inputPassword').val();
+
     console.log('click');
+    console.log($emailReg);
+    console.log($passwordReg);
     // redireccion a siguiente vista
-    // window.location.href = 'home.html';
+    $(location).attr('href', 'home.html');
     // Registro de Usuario (NUEVO)
     firebase.auth().createUserWithEmailAndPassword($emailReg, $passwordReg).catch(function(error) {
       // Handle Errors here.
