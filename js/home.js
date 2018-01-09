e92050435e8dbfb2ae1ce727f11ed50d807dbaa8
lucerohospina <lucerojhp@gmail.com>
2018-01-09 14:59:23 -0500

    agregando eventos y funciones a botones y textareas

--- js/home.js
+++ js/home.js
@@ -4,11 +4,40 @@ $(document).ready(function() {
   var $postsContainer = $('#posts-container');
 
   $postBtn.on('click', sharePost);
+  
+  $textArea.on('keyup', function() {
+    if ($textArea.val()) {
+      $postBtn.removeAttr('disabled');
+      $postBtn.css({'background': '#f7b617',
+        'color': '#2b2b2b',
+        'border': 'none'});
+    } else {
+      $postBtn.attr('disabled', true);
+    }
+  });
+  
 
+  // function activeBtn() {
+  //   if ($textArea.val()) {
+  //     $postBtn.removeAttr('disabled');
+  //     $postBtn.css({ 'background': '#f7b617'});
+  //   }
+  // }
+
+  // function inactiveBtn() {
+  //   $postBtn.attr('disabled', true);
+  //   $postBtn.css({ 'background': '#fff'});
+  // }
+  
   function sharePost() {
     console.log('ye!');
     console.log($textArea.val());
-    $postsContainer.append($textArea.val());
+    if ($textArea.val()) {
+      $postsContainer.prepend('<div class="new-post card"></div>');
+      $('.new-post').first().append($textArea.val());
+      $textArea.val('');
+      $textArea.focus();
+    } 
   }
 });
   
--- js/profile.js
+++ js/profile.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
   // Declarando variables
   var $signOutBtn = $('#sign-out');
+  var $textArea = $('#write-posts');
+  var $postBtn = $('#posts-btn');
+  var $postsContainer = $('#posts-container');
+  var $likeBtn = $('.like-btn');
 
   // Asociando eventos
-
+  $postBtn.on('click', sharePost);
   // Evento al boton de CERRAR SESION
   $signOutBtn.on('click', signingOut);
-
+  
   // Funciones
   function signingOut() {
     console.log('click');
@@ -17,4 +21,39 @@ $(document).ready(function() {
         console.log('Error');
       });
   }
+
+  // Previniendo que el formulario se envie (que no refresque la página)
+  $('#create-post').submit(function() {
+    return false;
+  });
+
+  // Asociando evento al text area
+  $textArea.on('keyup', function() {
+    if ($textArea.val()) {
+      $postBtn.removeAttr('disabled');
+      $postBtn.css({'background': '#f7b617',
+        'color': '#2b2b2b',
+        'border': 'none'});
+    } else {
+      $postBtn.attr('disabled', true);
+    }
+  });
+
+  // Funcion para postear lo que se escriba en el text area
+  function sharePost(event) {
+    console.log('ye!');
+    console.log($textArea.val());
+    if ($textArea.val()) {
+      $postsContainer.prepend('<div class="card mt-3"><div class="card-header"><small>Publicado por</small>Usuario</div><div class="card-body" id="appendLike"><p class="card-text new-post"></p></div></div>');
+      $('.new-post').first().append($textArea.val());
+      $('#appendLike').append('<button class="btn btn-secondary like-btn"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>Me gusta</button>');
+      $textArea.val('');
+      $textArea.focus();
+    } 
+  }
+
+  $likeBtn.on('click', function() {
+    console.log('hey!');
+    $likeBtn.toggleClass('btn-primary').toggleClass('btn-secondary');
+  });
 });
