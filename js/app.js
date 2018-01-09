911e5a93f526794afa372e1f42c7fa5a7fcd9c82
lucerohospina <lucerojhp@gmail.com>
2018-01-08 23:05:20 -0500

    comentando lineas en app js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
-  $('.redirect').click(goTo);
+// $(document).ready(function() {
+//   $('.redirect').click(goTo);
 
-  function goTo(event) {
-    $('body').fadeOut(1000, function() {
-      location.attr('href', $(event.target).attr('data-target') + '.html');
-    });
-  }
-});
+//   function goTo(event) {
+//     $('body').fadeOut(1000, function() {
+//       location.attr('href', $(event.target).attr('data-target') + '.html');
+//     });
+//   }
+// });
 
 // Initialize Firebase
 var config = {
