15e244952ba101e3aba2264c871a6bffe3284c1d
andrea-isabel <andrea19.93.15@gmail.com>
2018-01-09 19:02:18 -0500

    avance

--- js/app.js
+++ js/app.js
@@ -1,9 +1,85 @@
-$(document).ready(function() {
-  $('.redirect').click(goTo);
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyAuWjgjDXgNHkKnscxwhVpSK6G_p-IZX2s',
+  authDomain: 'codebook-cd8c9.firebaseapp.com',
+  databaseURL: 'https://codebook-cd8c9.firebaseio.com',
+  projectId: 'codebook-cd8c9',
+  storageBucket: 'codebook-cd8c9.appspot.com',
+  messagingSenderId: '42664775792'
+};
 
-  function goTo(event) {
-    $('body').fadeOut(1000, function() {
-      location.attr('href', $(event.target).attr('data-target') + '.html');
+firebase.initializeApp(config);
+
+// var user = null;
+var $loginGoogle = $('#google-login');
+var $loginFb = $('#fb-login');
+var $signOut = $('.signout');
+
+// login con Facebook
+var providerFb = new firebase.auth.FacebookAuthProvider();
+$loginFb.click(function() {
+  firebase.auth().signInWithPopup(providerFb).then(function(result) {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    firebase.database().ref('users/' + user.uid).set({
+      name: user.displayName,
+      email: user.email,
+      profilePhoto: user.photoURL,
     });
-  }
+    console.log(user);
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+});
+
+// login con Google
+var providerGoogle = new firebase.auth.GoogleAuthProvider();
+
+$loginGoogle.click(function() {
+  firebase.auth().signInWithPopup(providerGoogle).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    user = result.user;
+    console.log(user.displayName);
+    console.log(user.photoURL);
+    firebase.database().ref('users/' + user.uid).set({
+      name: user.displayName,
+      email: user.email,
+      profilePhoto: user.photoURL,
+
+    });
+    $(location).attr('href', 'home.html');
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+});
+
+
+// Cerrar sesión
+$signOut.click(function() {
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+    $(location).attr('href', 'login.html');
+  }).catch(function(error) {
+    // An error happened.
+  });
 });
--- js/profile.js
+++ js/profile.js
@@ -1,20 +1,32 @@
 $(document).ready(function() {
   // Declarando variables
   var $signOutBtn = $('#sign-out');
-
+  var $username = $('.displayUsername');
+  var $userEmail = $('#displayEmail');
+    
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var name = user.displayName;
+      var email = user.email;
+      var photoUrl = user.photoURL;
+      var emailVerified = user.emailVerified;
+      var uid = user.uid;
+      console.log(user);
+    } else {
+      // No user is signed in.
+    }
+  });
   // Asociando eventos
 
   // Evento al boton de CERRAR SESION
-  $signOutBtn.on('click', signingOut);
+  // $signOutBtn.on('click', signingOut);
+  displayInfo();
+  console.log(name);
 
   // Funciones
-  function signingOut() {
-    console.log('click');
-    firebase.auth().signOut().then(function() {
-      console.log('Saliendo...');
-    })
-      .catch(function() {
-        console.log('Error');
-      });
+  function displayInfo() {
+    $username.text(name);
+    $userEmail.text(email);
   }
 });
