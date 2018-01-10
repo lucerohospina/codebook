3c0c9608cbea2b9a1a5cab2a6e532029d5957542
lucerohospina <lucerojhp@gmail.com>
2018-01-10 04:37:37 -0500

    agregando boton de cerrar a los posts

--- js/home.js
+++ js/home.js
@@ -46,9 +46,10 @@ $(document).ready(function() {
     console.log('ye!');
     console.log($textArea.val());
     if ($textArea.val()) {
-      $postsContainer.prepend('<div class="card mt-3"><div class="card-header"><small>Publicado por</small> Usuario</div><div class="card-body" id="appendLike"><p class="card-text new-post"></p></div></div>');
+      $postsContainer.prepend('<div class="card mt-3"><div class="card-header btn-yellowLab"><small>Publicado por</small> Usuario</div><div class="card-body" id="appendLike"><p class="card-text new-post"></p></div></div>');
       $('.new-post').first().append($textArea.val());
       $('#appendLike').append('<button class="btn btn-secondary like-btn"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>Me gusta</button>');
+      $('.card-header').first().prepend('<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>');
       $textArea.val('');
       $textArea.focus();
       $postBtn.attr('disabled', true);
@@ -57,6 +58,13 @@ $(document).ready(function() {
 
   $(document).on('click', '.like-btn', function() {
     console.log('click success!');
-    $(this).toggleClass('btn-primary').toggleClass('btn-secondary');
+    $(this).toggleClass('btn-primary').toggleClass('btn-secondary').toggleClass('font-weight-bold');
   }); 
+
+  // $(document).on('click', '.close', function() {
+  //   console.log('close-click');
+  //   $('.del-post').addClass('collapse').collapse();
+  // });
+
+  // $('.collapse').collapse()
 });
